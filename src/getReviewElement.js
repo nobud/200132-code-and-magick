946403d1a7dae11eb9133e1b65c99c9073ebc3fc
nobud <nobud@yandex.ru>
2016-07-07 16:28:52 +0300

    объект для работы с данными и событие обновления данных

--- src/getReviewElement.js
+++ src/getReviewElement.js
@@ -1,26 +1,79 @@
 'use strict';
 
 (function() {
-  /** @enum {string} */
-  var ratingClasses = {
-    '1': 'review-rating',
-    '2': 'review-rating-two',
-    '3': 'review-rating-three',
-    '4': 'review-rating-four',
-    '5': 'review-rating-five'
+  var BaseComponent = require('./base');
+  var utils = require('./utils');
+
+  var ReviewElement = function(templateToClone, container, data) {
+    BaseComponent.call(this, templateToClone, container);
+
+    this.data = data;
+
+    /** @enum {string} */
+    this.ratingClasses = {
+      '1': 'review-rating',
+      '2': 'review-rating-two',
+      '3': 'review-rating-three',
+      '4': 'review-rating-four',
+      '5': 'review-rating-five'
+    };
+
+    /** @constant {number} */
+    this.LOAD_IMAGE_TIMEOUT = 10000;
+
+    /** @constant {string} */
+    this.CLASS_REVIEW_LOAD_FAILURE = 'review-load-failure';
+
+    this.reviewQuiz = null;
+
+    this.reviewUsefulness = null;
+
+    this.getReviewElement();
   };
-
-  /** @constant {number} */
-  var LOAD_IMAGE_TIMEOUT = 10000;
-
-  /** @constant {string} */
-  var CLASS_REVIEW_LOAD_FAILURE = 'review-load-failure';
+  utils.inherit(BaseComponent, ReviewElement);
 
   /**
    * @param {HTMLElement} element
    */
-  var errorLoadingReview = function(element) {
-    element.classList.add(CLASS_REVIEW_LOAD_FAILURE);
+  ReviewElement.prototype.errorLoadingReview = function(element) {
+    element.classList.add(this.CLASS_REVIEW_LOAD_FAILURE);
+  };
+
+  /**
+   * @param {HTMLElement} element
+   * @param {Image} authorImage
+   */
+  ReviewElement.prototype.errorLoadingReviewOnTimeOut = function(element,
+    authorImage) {
+    authorImage.src = '';
+    this.errorLoadingReview(element);
+  };
+
+  ReviewElement.prototype.getUsefulness = function() {
+    this.reviewUsefulness.textContent = '(' + this.data.getReviewUsefulness() +
+      ')';
+  };
+
+  ReviewElement.prototype.quizUpdate = function() {
+    this.getUsefulness();
+    this.activate();
+  };
+
+  ReviewElement.prototype.activate = function() {
+    this.reviewQuiz.classList.add('review-quiz-answer-active');
+    var answers = this.reviewQuiz.parentElement.querySelectorAll(
+      '.review-quiz-answer');
+    Array.prototype.forEach.call(answers, function(item) {
+      item.classList.add('review-quiz-answer-active');
+    });
+  };
+
+  ReviewElement.prototype.isYes = function(elem) {
+    return elem.classList.contains('review-quiz-answer-yes');
+  };
+
+  ReviewElement.prototype.isNot = function(elem) {
+    return elem.classList.contains('review-quiz-answer-no');
   };
 
   /**
@@ -28,38 +81,47 @@
    * @param {HTMLElement} container
    * @return {HTMLElement}
    */
-  var getReviewElement = function(data, container, templateToClone) {
-    var element = templateToClone.cloneNode(true);
+  ReviewElement.prototype.getReviewElement = function() {
+    var element = this.element.cloneNode(true);
+    this.reviewQuiz = element.querySelector('.review-quiz');
     var rating = element.querySelector('.review-rating');
-    rating.classList.add(ratingClasses[data.rating]);
     var reviewText = element.querySelector('.review-text');
-    var reviewQuiz = element.querySelector('.review-quiz');
     var imgDOMElement = element.querySelector('img.review-author');
     var authorImage = new Image();
     var imageLoadTimeout;
+
     authorImage.onload = function(evt) {
       clearTimeout(imageLoadTimeout);
       imgDOMElement.src = evt.target.src;
       imgDOMElement.width = 124;
       imgDOMElement.height = 124;
-      imgDOMElement.title = data.author.name;
+      imgDOMElement.title = this.data.getAuthorName();
     };
+    authorImage.onload = authorImage.onload.bind(this);
+
     authorImage.onerror = function() {
-      errorLoadingReview(element);
-      console.log('error loading image: ' + this.src);
+      clearTimeout(imageLoadTimeout);
+      this.errorLoadingReview(element);
     };
-    authorImage.src = data.author.picture;
-    imageLoadTimeout = setTimeout(function() {
-      authorImage.src = '';
-      errorLoadingReview(element);
-    }, LOAD_IMAGE_TIMEOUT);
-    reviewText.textContent = data.author.name + ' (' + data.date + '): ' +
-      data.description;
-    reviewQuiz.innerHTML = '(' + data.review_usefulness +
-      ')' + reviewQuiz.innerHTML;
-    return element;
+    authorImage.onerror = authorImage.onerror.bind(this);
+
+    authorImage.src = this.data.getAuthorPhotoSrc();
+
+    var onTimeOut = function() {
+      this.errorLoadingReviewOnTimeOut(element, authorImage);
+    };
+    imageLoadTimeout = setTimeout(onTimeOut, this.LOAD_IMAGE_TIMEOUT);
+
+    rating.classList.add(this.ratingClasses[this.data.getRating()]);
+    reviewText.textContent = this.data.getReviewText();
+
+    this.reviewUsefulness = document.createElement('span');
+    this.reviewUsefulness.className = 'review-usefulness';
+    this.getUsefulness();
+    this.reviewQuiz.insertBefore(this.reviewUsefulness, this.reviewQuiz.firstChild);
+
+    this.element = element;
   };
 
-  module.exports = getReviewElement;
-
+  module.exports = ReviewElement;
 })();
--- src/review.js
+++ src/review.js
@@ -4,29 +4,34 @@
 
   var BaseComponent = require('./base');
   var utils = require('./utils');
+  var ReviewElement = require('./getReviewElement');
 
   /**
    * @param {Object} data
    * @param {Element} container
    * @constructor
    */
-  var Review = function(data, container, templateToClone) {
+  var Review = function(templateToClone, container, data) {
+    BaseComponent.call(this, null, container);
     this.data = data;
-    var getReviewElement = require('./getReviewElement');
-    this.element = getReviewElement(this.data, container, templateToClone);
-    this.container = container;
+    this.objectElement = new ReviewElement(templateToClone, container,
+      this.data);
+    this.element = this.objectElement.element;
     this._onQuizClick = this._onQuizClick.bind(this);
-
+    this._onDataUsefulnessChange = this._onDataUsefulnessChange.bind(this);
     this.init();
+
   };
   utils.inherit(BaseComponent, Review);
 
   Review.prototype._setEventListeners = function() {
-    this.element.addEventListener('click', this._onQuizClick, true);
+    this.objectElement.reviewQuiz.addEventListener('click', this._onQuizClick);
+    window.addEventListener('dataChange', this._onDataUsefulnessChange);
   };
 
   Review.prototype._removeEventListeners = function() {
-    this.element.removeEventListener('click', this._onQuizClick);
+    this.objectElement.reviewQuiz.removeEventListener('click', this._onQuizClick);
+    window.removeEventListener('dataChange', this._onDataUsefulnessChange);
   };
 
   Review.prototype.init = function() {
@@ -34,9 +39,20 @@
   };
 
   Review.prototype._onQuizClick = function(evt) {
-    var e = evt.target;
-    if (e.classList.contains('review-quiz-answer')) {
-      e.classList.add('review-quiz-answer-active');
+    var elem = evt.target;
+
+    if (this.objectElement.isYes(elem)) {
+      this.data.upReviewUsefulness();
+    } else if (this.objectElement.isNot(elem)) {
+      this.data.downReviewUsefulness();
+    }
+  };
+
+  Review.prototype._onDataUsefulnessChange = function(evt) {
+    var sender = evt.detail.sender;
+    if (sender === this.data) {
+      this.objectElement.quizUpdate();
+      this.objectElement.reviewQuiz.removeEventListener('click', this._onQuizClick);
     }
   };
 
